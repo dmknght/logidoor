fb10f25a5cb36a16106ce6fa8830db295e99e49a
Nong Hoang Tu <dmknght@parrotsec.org>
2021-05-30 00:46:53 +0700

    Comment some unused functions

--- logidoor/new_libs/cores.py
+++ logidoor/new_libs/cores.py
@@ -1,31 +1,32 @@
-import random
+# import random
+#
+#
+# def rand_from_list(arg_list):
+#     """
+#     Select element in list randomly
+#     :param arg_list: a list of values
+#     :return: Random element in arg_list
+#     """
+#     return random.choice(arg_list)
+#
+#
+# def rand_from_file(file_location):
+#     """
+#     Select a line in a file randomly
+#     :param file_location: string = file location
+#     :return: a line in the file
+#     """
+#     return rand_from_list(file_read(file_location).split("\n"))
 
 
-def rand_from_list(arg_list):
-    """
-    Select element in list randomly
-    :param arg_list: a list of values
-    :return: Random element in arg_list
-    """
-    return random.choice(arg_list)
-
-
-def rand_from_file(file_location):
-    """
-    Select a line in a file randomly
-    :param file_location: string = file location
-    :return: a line in the file
-    """
-    return rand_from_list(file_read(file_location).split("\n"))
-
-
-def str_to_list(username):
-    """
-    Split input string by ':' value (use for username in options)
-    :param username: string = option users give from keyboard
-    :return: a list of username splitted by ':'
-    """
-    return username.split(":")
+# def str_to_list(username):
+#     """
+#     Split input string by ':' value (use for username in options)
+#     :param username: string = option users give from keyboard
+#     :return: a list of username splits by ':'
+#     """
+#     return username.split(":")
+_version_ = "0.0.1"
 
 
 def file_load(file_location):
@@ -86,27 +87,27 @@ def file_write_append(file_location, data):
         raise Exception(error)
 
 
-def rand_string(len_min=2, len_max=5, select_type="char"):
-    """
-    Generate a string randomly with random length
-    :param len_min: int[default] = 2 Min value of int range to choose randomly for text len
-    :param len_max: int[default] = 5 Max value of int range to choose randomly for text len
-    :param select_type: string = [ "char" | "dig" ] choose charset type
-    :return: random string
-    refer: https://stackoverflow.com/a/2257449
-    """
-    import string
-
-    # Generate charset range from select_type
-    charset = string.ascii_letters
-    if select_type == "dig":
-        charset = string.digits
-
-    # Generate length of string from len_min and len_max
-    len_min, len_max = 0, random.randint(len_min, len_max)
-
-    # return string value
-    return ''.join(random.choice(charset) for _ in range(len_min, len_max))
+# def rand_string(len_min=2, len_max=5, select_type="char"):
+#     """
+#     Generate a string randomly with random length
+#     :param len_min: int[default] = 2 Min value of int range to choose randomly for text len
+#     :param len_max: int[default] = 5 Max value of int range to choose randomly for text len
+#     :param select_type: string = [ "char" | "dig" ] choose charset type
+#     :return: random string
+#     refer: https://stackoverflow.com/a/2257449
+#     """
+#     import string
+#
+#     # Generate charset range from select_type
+#     charset = string.ascii_letters
+#     if select_type == "dig":
+#         charset = string.digits
+#
+#     # Generate length of string from len_min and len_max
+#     len_min, len_max = 0, random.randint(len_min, len_max)
+#
+#     # return string value
+#     return ''.join(random.choice(charset) for _ in range(len_min, len_max))
 
 
 def get_domain(url):
