e666c0e57213a84f9bec0d95be5f9382b8ac7fd1
Nong Hoang Tu <dmknght@parrotsec.org>
2021-05-30 01:15:22 +0700

    Save space for banner

--- logidoor/new_libs/cli/banner.py
+++ logidoor/new_libs/cli/banner.py
@@ -2,14 +2,13 @@ from logidoor.new_libs.cores import _version_
 
 
 def program_banner():
-    print("  _             _ _____                   ")
-    print(" | |           (_)  __ \\                      ")
-    print(f" | | ___   __ _ _| |  | | ___   ___  _ __    ---[    \033[95mWeb form login brute-force\033[0m  ]---")
-    print(f" | |/ _ \\ / _` | | |  | |/ _ \\ / _ \\| '__|   ---[{' ':9}Version: \033[97m{_version_}\033[0m{' ':9}]---")
-    print(f" | | (_) | (_| | | |__| | (_) | (_) | |      ---[{' ':9}License: \033[93mGPL-3\033[0m{' ':9}]---")
-    print(" |_|\\___/ \\__, |_|_____/ \\___/ \\___/|_|   ")
-    print("           __/ |                          ")
+    print("  _             _ _____")
+    print(" | |           (_)  __ \\")
+    print(f" | | ___   __ _ _| |  | | ___   ___  _ __   --[ \033[95mWeb form login brute-force\033[0m   ]--")
+    print(f" | |/ _ \\ / _` | | |  | |/ _ \\ / _ \\| '__|  --[ Author: \033[96mNông Hoàng Tú\033[0m{' ':8}]--")
+    print(f" | | (_) | (_| | | |__| | (_) | (_) | |     --[ Email: \033[37mdmknght@parrotsec.org\033[0m ]--")
+    print(f" |_|\\___/ \\__, |_|_____/ \\___/ \\___/|_|     --[ Version: \033[97m{_version_}\033[0m{' ':15}]--")
+    print(f"           __/ |{' ':28}--[ License: \033[93mGPL-3\033[0m{' ':15}]--")
     print("          |___/                           \n")
-    print("-----[ Author: \033[96mNông Hoàng Tú\033[0m ]---[ \033[37mdmknght@parrotsec.org\033[0m     ]-----")
-    print("-----[ \033[94mhttps://nest.parrotsec.org/packages/tools/logidoor\033[0m    ]-----")
-    print("-----[ Program is using \033[34mMechanicalSoup 1.0\033[0m under \033[33mLicense MIT\033[0m ]-----\n")
+    print("-----[ Gitlab: \033[94mhttps://nest.parrotsec.org/packages/tools/logidoor\033[0m  ]-----")
+    print("-----[ Program is using \033[37mMechanicalSoup 1.0\033[0m under \033[33mLicense MIT\033[0m       ]-----\n")
