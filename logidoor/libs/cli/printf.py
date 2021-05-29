39e4e04cd3480f865fd73ab7d65204e8d65e8428
Nong Hoang Tu <dmknght@parrotsec.org>
2021-05-30 03:11:54 +0700

    - Add print results - Add print_no_login_found with progressbar friendly - Add print_error with progressbar friendly

--- logidoor/libs/cli/printf.py
+++ logidoor/libs/cli/printf.py
@@ -14,6 +14,32 @@ def print_found(username, password):
         print(f"  [\033[97mFound\033[0m][Password: \"\033[95m{password}\033[0m\"]")
 
 
+def print_error(text):
+    __clean()
+    print(f"  [\033[91mERR\033[0m] {text}")
+
+
 def print_attack(url):
     __clean()
     print(f"Attacking \033[94m{url:65.65}\033[0m")
+
+
+def print_no_login_found(url):
+    __clean()
+    print(f"  [\033[91mATT\033[0m] Can't find login at \033[94m{url}\033[0m")
+
+
+def print_results(results):
+    __clean()
+    if not results:
+        print("\nResult: \033[91mNo login found\033[0m")
+    else:
+        print(f"\nResult: \033[97mFound {len(results)}\033[0m")
+        print("-" * 80)
+        for result in results:
+            url, username, password = result
+            print(f"URL: \033[94m{url}\033[0m")
+            if username:
+                print(f"Username: \"\033[96m{username}\033[0m\"")
+            print(f"Password: \"\033[95m{password}\033[0m\"")
+            print("-" * 80)
