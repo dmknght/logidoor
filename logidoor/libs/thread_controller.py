2a0c7cd241a2716c949196a156826ceb609361c2
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-01 03:52:29 +0700

    Fix a bug that other usernames are not checked if 1 is found

--- logidoor/libs/thread_controller.py
+++ logidoor/libs/thread_controller.py
@@ -1,6 +1,13 @@
 import threading
 
 
+def is_valid_to_add(url, username, result):
+    for check_values in list(result.queue):
+        if username in check_values and url in check_values:
+            return False
+    return True
+
+
 def run_threads(threads):
     for thread in threads:
         thread.start()
@@ -14,15 +21,16 @@ def setup_threads(browser, url, options, result, target):
     for username in options.userlist:
         for password in options.passlist:
             if len(workers) == options.threads:
-                # When login is found, we added URL to queue list
-                # We check the URL to detect if login is done
-                if url in [check_urls[0] for check_urls in list(result.queue)]:
-                    return
                 run_threads(workers)
                 del workers[:]
-            worker = threading.Thread(target=target, args=(browser, url, username, password, result))
-            worker.daemon = True
-            workers.append(worker)
+            # When login is found, we added URL and username for current url to queue list
+            # We check the URL and username to detect if login is done
+            # if url == check_url and username == check_username
+            if is_valid_to_add(url, username, result):
+                worker = threading.Thread(target=target, args=(browser, url, username, password, result))
+                worker.daemon = True
+                workers.append(worker)
+
     if workers:
         run_threads(workers)
         del workers[:]
